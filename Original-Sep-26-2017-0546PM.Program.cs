1fab5017a336757149f12b30114848779a76170b
DanielTonchev <32263403+DanielTonchev@users.noreply.github.com>
2017-10-02 12:37:54 +0300

    Add files via upload

---
+++ Original-Sep-26-2017-0546PM.Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                string str = new string('1', n);
+                Console.WriteLine(str);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
